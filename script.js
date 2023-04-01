2b0ba9e3ae2b29deba886926636e2c92c52d83d1
STEPANBARSIKOV <127612919+STEPANBARSIKOV@users.noreply.github.com>
2023-04-01 23:54:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -46,25 +46,3 @@ $('body').on("touchend", function (event){
         backward();
     }
 })
-// код для логина 
-const form = document.querySelector("form");
-const usernameInput = document.getElementById("username-input");
-const passwordInput = document.getElementById("password-input");
-
-// Обрабатываем отправку формы
-form.addEventListener("submit", function(event) {
-  // Отменяем стандартное поведение формы
-  event.preventDefault();
-
-  // Получаем введенный логин и пароль
-  const enteredUsername = usernameInput.value;
-  const enteredPassword = passwordInput.value;
-
-  // Выполняем проверку логина и пароля
-  if (enteredUsername === "ArthurMorgan" && enteredPassword === "PokerPlans") {
-    console.log("Вход выполнен успешно!");
-    window.open("index.html");
-  } else {
-    alert("Неверный логин или пароль!");
-  }
-});
